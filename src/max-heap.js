bd8a1fb0b28fef59968dfcf775a995069e5e2ffb
KirillFilipp <kirill_filipp@mail.ru>
2019-10-19 23:58:37 +0300

    feat: add part of solution

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,47 +2,52 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+
 	}
 
 	pop() {
-		
+		const args = [...arguments];
+		args.pop();
+		return args;
 	}
 
 	detachRoot() {
-		
+
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
 	}
 
 	size() {
-		
+		return this.length;
 	}
 
 	isEmpty() {
-		
+		return this.length < 1;
 	}
 
 	clear() {
-		
+		this.root.data = null;
+		this.parentNodes.data = null;
+		this.length = 0;
 	}
 
 	insertNode(node) {
-		
+
 	}
 
 	shiftNodeUp(node) {
-		
+
 	}
 
 	shiftNodeDown(node) {
-		
+
 	}
 }
 
